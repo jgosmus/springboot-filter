bc5be907fee0da97d8818f8a7edb8a0815ab2926
Francisco <jgosmus@gmail.com>
2024-04-13 02:53:40 +0200

    Filter registered with URL Pattern

--- src/main/java/net/way2code/filters/FilterConfig.java
+++ src/main/java/net/way2code/filters/FilterConfig.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class FilterConfig {
     @Bean
-    public FilterRegistrationBean<AuthFilter> loggingFilter(){
+    public FilterRegistrationBean<AuthFilter> authFilter(){
         FilterRegistrationBean<AuthFilter> registrationBean
                 = new FilterRegistrationBean<>();
 
