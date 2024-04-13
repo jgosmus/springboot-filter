e87f4e32056c0d15f1c165db183fe12162549b8c
Francisco <jgosmus@gmail.com>
2024-04-13 02:40:19 +0200

    AuthFilter with OncePerRequestFilter

--- src/main/java/net/way2code/filters/AuthFilter.java
+++ src/main/java/net/way2code/filters/AuthFilter.java
@@ -2,17 +2,21 @@ package net.way2code.filters;
 
 
 import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
 import java.io.IOException;
 
 @Slf4j
 @Order(2)
 @Component
-public class AuthFilter implements Filter {
+public class AuthFilter extends OncePerRequestFilter {
     @Override
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws ServletException, IOException {
         // Checks to auth user
         log.info("Print from AuthFilter");
         chain.doFilter(request, response);
